4b4b3721e1396165bcbbd86066f5b7cbba180df2
ziyacivan <yusufziyacivan@gmail.com>
2025-10-17 12:14:47 +0300

    adds system admin and system user roles to the user model, and migrations

--- users/choices.py
+++ users/choices.py
@@ -3,6 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserRole(models.TextChoices):
+    system_admin = "system_admin", _("System Admin")
+    system_user = "system_user", _("System User")
     owner = "owner", _("Owner")
     manager = "manager", _("Manager")
     employee = "employee", _("Employee")
