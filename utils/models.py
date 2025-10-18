38af3f736a36b385ec872b855b2d321a1fa8bae4
ziyacivan <yusufziyacivan@gmail.com>
2025-10-18 18:04:35 +0300

    adds attributes JSON field to the BaseModel and creates migrations for related models

--- utils/models.py
+++ utils/models.py
@@ -6,5 +6,7 @@ class BaseModel(models.Model):
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     updated_on = models.DateTimeField(_("updated on"), auto_now=True)
 
+    attributes = models.JSONField(_("attributes"), default=dict, blank=True)
+
     class Meta:
         abstract = True
