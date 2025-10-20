a71073397d8e5b1e78095747f626bec4c286cbf8
ziyacivan <yusufziyacivan@gmail.com>
2025-10-20 03:28:10 +0300

    fix(auth): remove unnecessary whitespace in send_reset_email_task function

--- auth/tasks.py
+++ auth/tasks.py
@@ -18,7 +18,7 @@ def send_verification_email_task(
 @shared_task
 def send_reset_email_task(user_id: int, reset_url: str):
     from users.models import User
-    
+
     email_service = EmailService()
     try:
         user = User.objects.get(id=user_id)
